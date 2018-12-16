556fb9aa1b5e1ca26b2cef4bd81808b03204e2ed
Tim Jones <mynameistim97@gmail.com>
2018-12-16 12:45:28 +1000

    General polish, added animation to banner header text

--- files/js/script.js
+++ files/js/script.js
@@ -20,7 +20,7 @@ $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
 
-$(document).ready(function () {
+function initialise() {
     // bind click event to all internal page anchors
     $('a[href*="#"]').on('click', function (e) {
         // prevent default action and bubbling
@@ -41,7 +41,10 @@ $(document).ready(function () {
     // if ($(window).width() < 800) {                 
     //   $('.navbar-default').addClass('smaller');
     // } 
-    if ($(this).scrollTop() > 157) {                 
+    if ($(this).scrollTop() > 157) {      
+      if ($(window).width() < 991) {               
+          // $('.title-block-spacer').css('top', '157px');
+      }                   
       $('.banner-container').css('visibility', 'none');
       // $('.logo-img').css('display', 'inline-block');
       $('.logo-img').css('top', '0px');
@@ -50,6 +53,9 @@ $(document).ready(function () {
       // $('.navbar-default').addClass('solid');
 
     } else {
+      if ($(window).width() < 991) {               
+          // $('.title-block-spacer').css('top', '210px');
+      }   
       $('.logo-img').css('top', '-450px');
       // $('.logo-img').css('display', 'none');
       $('.banner-container').css('visibility', 'block');
@@ -65,20 +71,28 @@ $(document).ready(function () {
         });
         $('.jarallax-sml').jarallax({
           speed: 0.5
-        });
-        document.getElementById('jarallax-container-0').style.zIndex="unset";
-        document.getElementById('jarallax-container-1').style.zIndex="unset";
-      }, 700
+        });   
+        if(is_safari || is_edge_or_ie){
+          document.getElementById('jarallax-container-0').style.zIndex="unset";
+          document.getElementById('jarallax-container-1').style.zIndex="unset";
+        }     
+      }, 800
     );
     
-});
+}
 
+$(document).ready(function () {
+  initialise();
+});
 
 $(window).scroll(function() {
     // if ($(window).width() < 991) {                 
     //   $('.navbar-default').addClass('smaller');
     // } 
-    if ($(this).scrollTop() > 157) {                 
+    if ($(this).scrollTop() > 157) { 
+      if ($(window).width() < 991) {               
+          // $('.title-block-spacer').css('top', '157px');
+      }                 
       $('.banner-container').css('visibility', 'none');
       // $('.logo-img').css('display', 'inline-block');
       $('.logo-img').css('top', '0px');
@@ -86,6 +100,9 @@ $(window).scroll(function() {
       $('.title-block-main').css('top', '0px');
       // $('.navbar-default').addClass('solid');
     } else {
+      if ($(window).width() < 991) {               
+          // $('.title-block-spacer').css('top', '210px');
+      }   
       $('.logo-img').css('top', '-450px');
       // $('.logo-img').css('display', 'none');
       $('.banner-container').css('visibility', 'block');
@@ -93,26 +110,14 @@ $(window).scroll(function() {
       $('.title-block-main').css('top', '-53px');
       // $('.navbar-default').removeClass('solid');
     } 
+
 });
 //Check if window is resized to less than desktop res width, 
 //If it is, make navbar opaque
 
 $(window).resize(function () {
-    if ($(window).width() < 991) {                 
-      // $('.navbar-default').addClass('solid');
-      $('.navbar-default').addClass('smaller');
-    } 
-    else if ($(this).scrollTop() > 50) {                 
-      $('.navbar-default').addClass('smaller');
-      // $('.navbar-default').addClass('solid');
-
-    } else {
-      $('.navbar-default').removeClass('smaller');
-      // $('.navbar-default').removeClass('solid');
-    } 
     $('.jarallax').jarallax('destroy');
     $('.jarallax-sml').jarallax('destroy');
-    console.log('resized');
     $('.jarallax-img').css('width', $(window).width());
     $('.jarallax').jarallax({
         speed: 0.2
@@ -120,6 +125,11 @@ $(window).resize(function () {
     $('.jarallax-sml').jarallax({
         speed: 0.5
     });
+    initialise();
+    if(is_safari || is_edge_or_ie){
+      document.getElementById('jarallax-container-0').style.zIndex="unset";
+      document.getElementById('jarallax-container-1').style.zIndex="unset";
+    }
 });
 
 
@@ -133,9 +143,9 @@ $('.center').slick({
   centerPadding: '60px',
   slidesToShow: 1,
   slidesToScroll: 1,
-  // autoplay: false
-  autoplay: true,
-  autoplaySpeed: 3000
+  autoplay: false
+  // autoplay: true,
+  // autoplaySpeed: 3000
 });
 //Script to fade out the post when scrolled past
 // $(window).scroll(function () {
@@ -174,28 +184,14 @@ $(window).scroll(function () {
 
 
 
-// $(window).scroll(function() {
-//   //Detect browser and disable fixed backgrounds if on Safari or IE/Edge
-//     var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
-//     var is_safari = navigator.userAgent.indexOf("Safari") > -1;
-//     var is_edge_or_ie;    
-    
-//     var ua = window.navigator.userAgent;
-//     var trident = ua.indexOf('Trident/');
-//     var edge = ua.indexOf('Edge/');
-//     if (trident > 0 || edge > 0) {
-//       is_edge_or_ie = true;
-//     }
-//     if ((is_chrome)&&(is_safari)) {
-//       is_safari=false;
-//     }
-//     if(is_safari || is_edge_or_ie){
-//       $('html').addClass('touch');
-//     }
-//     else{
-//       $('html').removeClass('touch');
-//     }
-// });
+$(window).scroll(function() {
+    if(is_safari || is_edge_or_ie){
+      document.getElementById('jarallax-container-0').style.zIndex="unset";
+      document.getElementById('jarallax-container-1').style.zIndex="unset";
+    }
+    else{
+    }
+});
 
 Pace.restart();
 Pace.on("done", function(){
@@ -204,6 +200,9 @@ Pace.on("done", function(){
       {
        $('.loading').slideUp("slow", function() {
         });
+        // Only show main text banner after loading complete
+        $('.title-main').css('display', 'block');
       }, 500
     );
+
 });
