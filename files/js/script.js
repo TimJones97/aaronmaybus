b4e7279e8a2354dbb09962a926160a6be4aa5e6a
Tim Jones <mynameistim97@gmail.com>
2018-12-18 21:41:26 +1000

    Disable parallax on Edge due to performance issues, enable parallax on mobile

--- files/js/script.js
+++ files/js/script.js
@@ -6,6 +6,13 @@ var ua = window.navigator.userAgent;
 var trident = ua.indexOf('Trident/');
 var edge = ua.indexOf('Edge/');
 var spacerHeight = $('.title-block-spacer').height();  
+var checkMobile = false;
+
+window.mobilecheck = function() {
+var checkMobile = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return checkMobile;
+};
 
 jQuery.expr[':'].regex = function(elem, index, match) {
     var matchParams = match[3].split(','),
@@ -78,33 +85,18 @@ function initialise() {
     if ((is_chrome)&&(is_safari)) {
       is_safari=false;
     }
-    // if ($(window).width() < 800) {                 
-    //   $('.navbar-default').addClass('smaller');
-    // } 
     if ($(this).scrollTop() > 157) {      
-      if ($(window).width() < 991) {               
-          // $('.title-block-spacer').css('top', '157px');
-      }                   
       $('.banner-container').css('visibility', 'none');
-      // $('.logo-img').css('display', 'inline-block');
       $('.logo-img').css('top', '0px');
       $('.navbar-default').addClass('smaller');
-      $('.title-block-main').css('top', '0px');
-      // $('.navbar-default').addClass('solid');
-
+      $('.title-block-main').css('top', '0px');     
     } else {
-      if ($(window).width() < 991) {               
-          // $('.title-block-spacer').css('top', '210px');
-      }   
       $('.logo-img').css('top', '-450px');
-      // $('.logo-img').css('display', 'none');
       $('.banner-container').css('visibility', 'block');
       $('.navbar-default').removeClass('smaller');
       $('.title-block-main').css('top', '-53px');
-      // $('.navbar-default').removeClass('solid');
     } 
     $('.center').slick({
-      // centerMode: true,
       centerPadding: '60px',
       slidesToShow: 1,
       slidesToScroll: 1,
@@ -119,9 +111,12 @@ function initialise() {
       speed: 0.5
     });   
     $("div:regex(id, .*jarallax-container-.*)").css({"z-index": "unset"});
-    if(is_safari || is_edge_or_ie){
+    // Check if not on Chrome, and if on Edge/Safari not on mobile
+    // If on mobile keep parallax enabled
+    if((is_safari || is_edge_or_ie) && !mobilecheck()){
       $('.jarallax-img').addClass('edge-compatibility');
     }
+
     $('body').scrollspy({
       target: '#topnav',
       offset: 50
@@ -133,13 +128,7 @@ $(document).ready(function () {
 });
 
 $(window).scroll(function() {
-    // if ($(window).width() < 991) {                 
-    //   $('.navbar-default').addClass('smaller');
-    // } 
     if ($(this).scrollTop() > 157) { 
-      if ($(window).width() < 991) {               
-          // $('.title-block-spacer').css('top', '157px');
-      }                 
       $('.banner-container').css('visibility', 'none');
       // $('.logo-img').css('display', 'inline-block');
       $('.logo-img').css('top', '0px');
@@ -147,24 +136,16 @@ $(window).scroll(function() {
       $('.title-block-main').css('top', '0px');
       // $('.navbar-default').addClass('solid');
     } else {
-      if ($(window).width() < 991) {               
-          // $('.title-block-spacer').css('top', '210px');
-      }   
       $('.logo-img').css('top', '-450px');
-      // $('.logo-img').css('display', 'none');
       $('.banner-container').css('visibility', 'block');
       $('.navbar-default').removeClass('smaller');
       $('.title-block-main').css('top', '-53px');
-      // $('.navbar-default').removeClass('solid');
     } 
 });
-//Check if window is resized to less than desktop res width, 
-//If it is, make navbar opaque
 
 $(window).resize(function () {
     $('.jarallax').jarallax('destroy');
-    $('.jarallax-sml').jarallax('destroy');
-    $('.jarallax-img').css('width', $(window).width());
+    // $('.jarallax-img').css('width', $(window).width());
     $('.jarallax').jarallax({
         speed: 0.2
     });
@@ -229,7 +210,7 @@ Pace.restart();
 Pace.on("done", function(){
    var y = $(window).scrollTop();  //your current y position on the page
    $(window).scrollTop(y+1);
-   $(window).scrollTop();
+   // $(window).scrollTop();
    $('.loading').slideUp("slow", function() {
     });
     // Only show main text banner after loading complete
